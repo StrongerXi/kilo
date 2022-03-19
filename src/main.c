a1344a808a840db57c44b44b1dd1e10110825548
Ryan <ryan.guo99@gmail.com>
2022-03-19 12:03:10 -0500

    [Stage 2] Also exit on reading 'q'

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 // Return 0 on EOF
-static int _read_one_char_from_stdin() {
-  char c;
-  return read(STDIN_FILENO, &c, 1);
+static int _read_one_char_from_stdin(char* ch) {
+  return read(STDIN_FILENO, ch, 1);
 }
 
 int main() {
-  while (_read_one_char_from_stdin());
+  char ch;
+  while (_read_one_char_from_stdin(&ch) && ch != 'q');
   return 0;
 }
