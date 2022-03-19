ce9e23426feb740b0691e244dbd49fbafb6e7240
Ryan <ryan.guo99@gmail.com>
2022-03-19 12:41:30 -0500

    [Stage 2] Enable raw mode, i.e., no echo, no wait till ENTER key

--- src/main.c
+++ src/main.c
@@ -1,12 +1,32 @@
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
 
+static struct termios _original_termios;
+
+static void _disable_terminal_raw_mode() {
+  // ignore pending input, flush all output
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &_original_termios);
+}
+
+static void _enable_terminal_raw_mode() {
+  tcgetattr(STDIN_FILENO, &_original_termios);
+  atexit(_disable_terminal_raw_mode); // reset at program exit
+
+  struct termios raw = _original_termios;
+  // don't echo input, don't wait till ENTER key to process input
+  raw.c_lflag &= ~(ECHO | ICANON);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 // Return 0 on EOF
-static int _read_one_char_from_stdin(char* ch) {
-  return read(STDIN_FILENO, ch, 1);
+static void _process_user_input_from_stdin() {
+  char ch;
+  while (read(STDIN_FILENO, &ch, 1) && ch != 'q');
 }
 
 int main() {
-  char ch;
-  while (_read_one_char_from_stdin(&ch) && ch != 'q');
+  _enable_terminal_raw_mode();
+  _process_user_input_from_stdin();
   return 0;
 }
