a222884701d7734ba239fa16e47e9828badb7b5b
Ryan <ryan.guo99@gmail.com>
2022-03-19 15:07:30 -0500

    [Stage 2] Print error message and exit on any error

--- src/main.c
+++ src/main.c
@@ -1,18 +1,43 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+static void _perror_and_exit(const char *s) {
+  perror(s);
+  exit(1);
+}
+
+static void _tcgetattr_or_err(int fd, struct termios* termios) {
+  if (tcgetattr(fd, termios) == -1) {
+    _perror_and_exit("tcgetattr");
+  }
+}
+
+static void _tcssetattr_or_err(int fd, int flags, const struct termios* termios) {
+  if (tcsetattr(fd, flags, termios) == -1) {
+    _perror_and_exit("tcsetattr");
+  }
+}
+
+static void _read_or_err(int fd, void* buf, size_t count) {
+  // In Cygwin, when read() times out it returns -1 with an errno of EAGAIN
+  if (read(fd, buf, count) == -1 && errno != EAGAIN) {
+    _perror_and_exit("tcsetattr");
+  }
+}
+
 static struct termios _original_termios;
 
 static void _disable_terminal_raw_mode() {
   // ignore pending input, flush all output
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &_original_termios);
+  _tcssetattr_or_err(STDIN_FILENO, TCSAFLUSH, &_original_termios);
 }
 
 static void _enable_terminal_raw_mode() {
-  tcgetattr(STDIN_FILENO, &_original_termios);
+  _tcgetattr_or_err(STDIN_FILENO, &_original_termios);
   atexit(_disable_terminal_raw_mode); // reset at program exit
 
   struct termios raw = _original_termios;
@@ -32,14 +57,14 @@ static void _enable_terminal_raw_mode() {
       );
   raw.c_cc[VMIN] = 1;    // `read` on terminal return immediately on any input
   raw.c_cc[VTIME] = 100; // `read` on terminal input waits for at most 0.1s
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  _tcssetattr_or_err(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 // Return 0 on EOF
 static void _process_user_input_from_stdin() {
   while (1) {
     char ch = '\0';
-    read(STDIN_FILENO, &ch, 1);
+    _read_or_err(STDIN_FILENO, &ch, 1);
     if (iscntrl(ch)) {
       printf("%d\r\n", ch);
     } else {
