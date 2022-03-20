8903abdedad08e814f77b748a96bbc256f612d6c
Ryan <ryan.guo99@gmail.com>
2022-03-19 19:05:02 -0500

    [Stage 3] Use byte buffer to flush screen update all at once

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -10,14 +11,58 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 typedef struct {
+  char* buf;
+  unsigned int size;
+  unsigned int capacity;
+} byte_buf_t;
+
+typedef struct {
   int screen_rows;
   int screen_cols;
   int input_fd;
   int output_fd;
+  byte_buf_t* paint_buf;
 } editor_state_t;
 
 static void _perror_and_exit(const char *s);
 static void _clean_up_before_exit();
+
+static byte_buf_t* _new_byte_buf() {
+  byte_buf_t* buf = (byte_buf_t*) malloc(sizeof(byte_buf_t));
+  if (buf == NULL) {
+    return NULL;
+  }
+  buf->size = 0;
+  buf->capacity = 512;
+  buf->buf = (char*) malloc(buf->capacity);
+  if (buf->buf == NULL) {
+    free(buf);
+    return NULL;
+  }
+  return buf;
+}
+
+static void _append_byte_buf(byte_buf_t* buf, char* data, int count) {
+  if (buf->size + count > buf->capacity) {
+    buf->capacity *= 2;
+    buf->buf = realloc(buf->buf, buf->capacity);
+  }
+  memcpy(buf->buf + buf->size, data, count);
+  buf->size += count;
+}
+
+static void _clear_byte_buf(byte_buf_t* buf) {
+  buf->size = 0;
+}
+
+static int _flush_byte_buf(byte_buf_t* buf, int fd) {
+  return write(fd, buf->buf, buf->size);
+}
+
+static void _free_byte_buf(byte_buf_t* buf) {
+  free(buf->buf);
+  free(buf);
+}
 
 static void _tcgetattr_or_err(int fd, struct termios* termios) {
   if (tcgetattr(fd, termios) == -1) {
@@ -40,17 +85,9 @@ static int _read_or_err(int fd, void* buf, size_t count) {
   return ret;
 }
 
-static int _write_or_err(int fd, void* buf, size_t count) {
-  int ret = write(fd, buf, count);
-  if (ret == -1) {
-    _perror_and_exit("write");
-  }
-  return ret;
-}
-
-  // look up VT100 escape sequences
+// look up VT100 escape sequences
 static void _write_clear_screen(int fd) {
-  _write_or_err(fd, "\x1b[2J", 4);
+  write(fd, "\x1b[2J", 4);
 }
 
 // Return 0 on success
@@ -60,17 +97,20 @@ static int _write_set_cursor_pos(int out_fd, int row, int col) {
   return write(out_fd, buf, count) != count;
 }
 
-static void _write_set_cursor_to_topleft(int fd) {
-  if (_write_set_cursor_pos(fd, 1, 1)) {
-    _perror_and_exit("_write_set_cursor_to_topleft");
-  }
+// TODO better to use a paint_command buffer, with emulated ADTs
+static void _append_clear_screen(byte_buf_t* buf) {
+  _append_byte_buf(buf, "\x1b[2J", 4);
 }
 
-static void _write_draw_rows(const editor_state_t* state) {
+static void _append_set_cursor_to_topleft(byte_buf_t* buf) {
+  _append_byte_buf(buf, "\x1b[H", 4); // default to (1, 1)
+}
+
+static void _append_draw_rows(const editor_state_t* state) {
   for (int y = 1; y <= state->screen_rows; y++) {
-    write(state->output_fd, "~", 1);
+    _append_byte_buf(state->paint_buf, "~", 1);
     if (y < state->screen_rows) { // prevent forced terminal scrolling
-      write(state->output_fd, "\r\n", 2);
+      _append_byte_buf(state->paint_buf, "\r\n", 2);
     }
   }
 }
@@ -83,7 +123,7 @@ static void _perror_and_exit(const char *s) {
 
 static void _clean_up_before_exit() {
   _write_clear_screen(STDOUT_FILENO);
-  _write_set_cursor_to_topleft(STDOUT_FILENO);
+  _write_set_cursor_pos(STDOUT_FILENO, 1, 1);
 }
 
 static struct termios _original_termios;
@@ -133,10 +173,12 @@ static void _process_one_key_press(editor_state_t* state) {
 }
 
 static void _refresh_screen(const editor_state_t* state) {
-  _write_clear_screen(state->output_fd);
-  _write_set_cursor_to_topleft(state->output_fd);
-  _write_draw_rows(state);
-  _write_set_cursor_to_topleft(state->output_fd);
+  _append_clear_screen(state->paint_buf);
+  _append_set_cursor_to_topleft(state->paint_buf);
+  _append_draw_rows(state);
+  _append_set_cursor_to_topleft(state->paint_buf);
+  _flush_byte_buf(state->paint_buf, state->output_fd);
+  _clear_byte_buf(state->paint_buf);
 }
 
 int _write_get_cursor_pos(int in_fd, int out_fd, int *row, int *col) {
@@ -177,21 +219,35 @@ static void _get_screen_size(int in_fd, int out_fd, int* rows, int* cols) {
   }
 }
 
-static void _init_editor_state(editor_state_t* state) {
+static editor_state_t* _new_editor_state() {
+  editor_state_t* state = (editor_state_t*) malloc(sizeof(editor_state_t));
+  if (state == NULL) {
+    _perror_and_exit("_new_editor_state");
+  }
   state->input_fd = STDIN_FILENO;
   state->output_fd = STDOUT_FILENO;
   _get_screen_size(
     state->input_fd, state->output_fd, &state->screen_rows, &state->screen_cols
   );
+  state->paint_buf = _new_byte_buf();
+  if (state->paint_buf == NULL) {
+    _perror_and_exit("_init_editor_state -> _new_byte_buf");
+  }
+  return state;
+}
+
+static void _free_editor_state(editor_state_t* state) {
+  _free_byte_buf(state->paint_buf);
+  free(state);
 }
 
 int main() {
   _enable_terminal_raw_mode();
-  editor_state_t state;
-  _init_editor_state(&state);
+  editor_state_t* state = _new_editor_state();
   while (1) {
-    _refresh_screen(&state);
-    _process_one_key_press(&state);
+    _refresh_screen(state);
+    _process_one_key_press(state);
   }
+  _free_editor_state(state);
   return 0;
 }
